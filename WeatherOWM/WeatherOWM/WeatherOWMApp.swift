39f2ea06de58b9cca3bf792d0bfb134852311944
SideDevOfKuma <5934440+SideDevOfKuma@users.noreply.github.com>
2026-01-07 13:12:55 -0300

    Project creation

---
+++ WeatherOWM/WeatherOWM/WeatherOWMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherOWMApp.swift
+//  WeatherOWM
+//
+//  Created by Lucas Paintner on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherOWMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherOWM/WeatherOWMTests/WeatherOWMTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherOWMTests.swift
+//  WeatherOWMTests
+//
+//  Created by Lucas Paintner on 07/01/2026.
+//
+
+import Testing
+@testable import WeatherOWM
+
+struct WeatherOWMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
